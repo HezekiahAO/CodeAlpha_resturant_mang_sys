46c47fa335e11443822ccc5f4e013f67a733dc6a
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2026-05-23 23:07:54 +0100

    Comment

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_system.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc:        # If there is a faliure to import Django, it will raise an ImportError with a message guiding the user to check their installation and environment setup.
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
